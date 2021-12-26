ec5bd50332fb1a698bb7ec31057b7d1af5cd2593
Mohamed Feddad <mrf345@gmail.com>
2021-12-26 15:40:27 +0400

    Replace local getSettings with extensionUtils

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,3 @@
-const { Gio } = imports.gi
-const Me = imports.misc.extensionUtils.getCurrentExtension()
-
 // eslint-disable-next-line no-unused-vars, no-var
 var Window = {
   isMaximized: w => w && w.maximized_vertically && w.maximized_horizontally,
@@ -20,17 +17,4 @@ var Workspace = {
     return Array(global.workspace_manager.get_n_workspaces()).fill()
       .map(() => global.workspace_manager.get_workspace_by_index(++i))
   }
-}
-
-// eslint-disable-next-line no-unused-vars, no-var
-var getSettings = (schemaKey = 'org.gnome.shell.extensions.maximize-into-empty-workspace') => {
-  const schemaSource = Gio.SettingsSchemaSource.new_from_directory(
-    Me.dir.get_child('schemas').get_path(),
-    Gio.SettingsSchemaSource.get_default(),
-    false
-  )
-  const schemaObj = schemaSource.lookup(schemaKey, true)
-
-  if (!schemaObj) throw new Error(`Schema not found "${schemaKey}"`)
-  return new Gio.Settings({ settings_schema: schemaObj })
 }
